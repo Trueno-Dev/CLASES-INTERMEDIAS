e15236fe3fb6de13d3d5b1b13972cf9468e46c19
Fernando <carlos_meca17@hotmail.com>
2024-12-02 13:22:37 -0500

    Actualizando lo aprendido en Python.

--- 00_dates.py
+++ 00_dates.py
@@ -1,6 +1,7 @@
 ### Dates ###
 
-from datetime import datetime
+from datetime import datetime, time, date, timedelta
+from turtledemo.penrose import start
 
 now = datetime.now()
 
@@ -15,6 +16,39 @@ def print_date (date):
 
 print_date(now)
 
-year_2025 = datetime(2025, 1, 1)
+year_2025 = datetime(2025, 1, 1) # Fecha y Hora Manual
 
 print_date(year_2025)
+
+current_time = time(21, 6, 0) # Fecha Manual
+
+print(current_time.hour)
+print(current_time.minute)
+print(current_time.second)
+
+current_date = date.today() # Día Automático.
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+current_date = date(2025, 10, 6) # Día Manual
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+current_date = date(current_date.year, current_date.month + 1, current_date.day)
+print(current_date.month)
+
+diff = year_2025 - now
+print(diff)
+
+diff = year_2025.date() - current_date
+print(diff)
+
+start_timedelta = timedelta(200, 100, 100, weeks = 10)
+end_timedelta = timedelta(300, 100, 100, weeks = 13)
+
+print(end_timedelta - start_timedelta)
+print(end_timedelta + start_timedelta)
